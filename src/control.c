f3ec8bdceca2d214b1cbf447d99897ea65b48ab8
Oran Ellis <oran.mcnair@gmail.com>
2022-07-20 22:06:21 +0100

    Started control logic

---
+++ src/control.c
@@ -0,0 +1,22 @@
+#define N_POLES 2
+#define V_S_FALLBACK 400
+
+void generatePWMs() {
+
+int va = 0;
+int vb = 0;
+int vc = 0;
+
+// Target phase voltages for phases a, b and c
+
+int v1; int pw1;
+int v2; int pw2;
+int v3; int pw3;
+
+double vCommand;
+
+// what is the order of the windings in a motor, does it matter?
+
+// The stator voltages a, b, and c; terminal voltage
+
+}
