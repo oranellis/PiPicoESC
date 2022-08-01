202ed1df8617325c8d78f69305452c6ae334f086
Karl Hartmann <karlhartmann03@gmail.com>
2022-08-02 00:39:57 +0100

    Incremental changes

--- src/controller.c
+++ src/controller.c
@@ -1,29 +1,100 @@
+#include "controllerMaths.h"
+#include "interfacing.h"
+#include "enums.h"
+
+#include <stdio.h>
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "hardware/pwm.h"
-#include "controllerMaths.h"
+volatile int fieldRotPos = 0; // Clamped between 0 and 1023 with datum at phase 1 positive current magnetic field orientation
+volatile int throttle = 0; // Between 0 and 32767
+volatile int fieldSpinSpeed = 2048; //256*revs per second
+volatile enum states state = s_fault;
+volatile uint64_t timeshitter;
+volatile long phaseTime = 0; //time in us through the phase rotation
+volatile long phasePeriod = 4000000; // period of phase rotation (spinnning speeeeeeeeeeeeeeeeeeeeeeeed)
+volatile long timediff = 0;
+
+void powerLoop() {
+    while (1) {
+        state = s_fault;
+
+
+        if (throttle <= 0) {
+            state = s_idle;
+            break;
+        }
+
+        if (state == s_fault) {
+            break;
+        }
+    }
+}
+
+
+void regenLoop() {
+    while (1) {
+        state = s_fault;
+
+        if (throttle >= 0) {
+            state = s_idle;
+        }
+
+        if (state == s_fault) {
+            break;
+        }
+    }
+}
+
+volatile int phaseA, phaseB, phaseC;
+volatile double phasePos;
+volatile long phasePos1024;
+
+void faultLoop() {
+    timediff = time_us_64() - timeshitter;
+    timeshitter = time_us_64();
+    phaseTime = (phaseTime + timediff) % phasePeriod;
+    phasePos = (phaseTime / phasePeriod);
+    phasePos1024 = (long) phasePos * 1024;
+    phaseA = fastsin[(phasePos1024)%1024];
+    phaseB = fastsin[(phasePos1024+341)%1024];
+    phaseC = fastsin[(phasePos1024+683)%1024];
+    // printf("A: %i B: %i C: %i\n", phaseA, phaseB, phaseC);
+    printf("C: %i\n", timeshitter);
+    sleep_ms(1);
+}
+
+
+
 
 int main() {
 
-    // IO Initialisation 
-    stdio_init_all();
+    // =================== Interfacing setup ===================
+    if (iosetup() != 0) {
+        printf("eFailed to initialise IO");
+        return 1;
+    }
 
-    // LED setup
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+    // =================== Global variables ===================
+    // Motor state variables
+  
 
-    // PWM setup
-    pwm_config cfg = pwm_get_default_config();
-    gpio_set_function(22, GPIO_FUNC_PWM);
-    int slice = pwm_gpio_to_slice_num(22);
-    pwm_set_gpio_level(22, 32768);
-    pwm_set_enabled(22, true);
+    printf("State number: %i\n", state);
 
-    // LED loop
-    while (true) {
-        for (int i =0;i<1023;i++) {
-            printf("i: %i, sin(i): %i", i, fastsin[i]);
-            sleep_us(500);
+    // Main event loop
+
+    while (1) {
+        
+        switch (state) {
+            // case s_fault: faultLoop(); break;
+            // case s_init: init(); break;
+            // case s_idle: idleLoop(); break;
+            // case s_ready: readyLoop(); break;
+            // case s_power: powerLoop(); break;
+            // case s_regen: regenLoop(); break;
+            default: faultLoop(); break;
         }
     }
+
+    return 0;
 }
--- src/interfacing.h
+++ src/interfacing.h
@@ -1,17 +1,25 @@
 #include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/pwm.h"
+#define PICO_COMPILED
 
-int main() {
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
+int iosetup() {
+    
+    #ifdef PICO_COMPILED
+     // IO Initialisation 
+    stdio_init_all();
+
+    // LED setup
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-    while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(100);
-    }
-#endif
+
+    // PWM setup
+    pwm_config cfg = pwm_get_default_config();
+    gpio_set_function(22, GPIO_FUNC_PWM);
+    int slice = pwm_gpio_to_slice_num(22);
+    pwm_set_gpio_level(22, 32768);
+    pwm_set_enabled(22, true);
+
+    return 0;
+    #endif
 }
