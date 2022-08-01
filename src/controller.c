0ee59e459488bfb3671019bc06c077aab994ba50
Oran Ellis <oran.mcnair@gmail.com>
2022-08-01 11:34:04 +0100

    Serial USB writing and sin function

--- src/controller.c
+++ src/controller.c
@@ -1,16 +1,19 @@
-#include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include "pico/time.h"
 #include "hardware/pwm.h"
+#include "controllerMaths.h"
 
 int main() {
+
+    // IO Initialisation 
+    stdio_init_all();
+
     // LED setup
     const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
     // PWM setup
-    gpio_init(22);
+    pwm_config cfg = pwm_get_default_config();
     gpio_set_function(22, GPIO_FUNC_PWM);
     int slice = pwm_gpio_to_slice_num(22);
     pwm_set_gpio_level(22, 32768);
@@ -18,9 +21,9 @@ int main() {
 
     // LED loop
     while (true) {
-        gpio_put(LED_PIN, 1);
-        sleep_ms(100);
-        gpio_put(LED_PIN, 0);
-        sleep_ms(100);
+        for (int i =0;i<1023;i++) {
+            printf("i: %i, sin(i): %i", i, fastsin[i]);
+            sleep_us(500);
+        }
     }
 }
