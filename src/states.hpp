115afa4a1d438fedc15c76e5ffff63de1838049d
Oran Ellis <oran.mcnair@gmail.com>
2022-08-16 02:54:23 +0100

    Added back renamed header files

---
+++ src/states.hpp
@@ -0,0 +1,54 @@
+//  ____  _ ____  _           _____ ____   ____
+// |  _ \(_)  _ \(_) ___ ___ | ____/ ___| / ___|
+// | |_) | | |_) | |/ __/ _ \|  _| \___ \| |
+// |  __/| |  __/| | (_| (_) | |___ ___) | |___
+// |_|   |_|_|   |_|\___\___/|_____|____/ \____|
+//
+// ==== states.h ====
+//
+// Header file for states.cpp
+//
+// Authors: Oran Ellis, Karl Hartmann
+// Licence: GPLv3
+
+#include "interfacing.h"
+
+enum class States {
+    kFault,
+    kInit,
+    kReady,
+    kRegen,
+    kCharging,
+    kIdle,
+    kPower
+};
+
+enum class DriveModes {
+    kPark,
+    kReverse,
+    kNeutral,
+    kEco,
+    kDrive,
+    kSpeeeeeed,
+};
+
+
+void Init();
+
+
+void IdleLoop();
+
+
+void ReadyLoop();
+
+
+void PowerLoop(Interface* interface);
+
+
+void RegenLoop();
+
+
+void FaultLoop();
+
+
+void ChargingLoop();
